7f6c17da99bf3053d0e1bf407785fc92a6b951bf
Taylor Dawson <taylorjdawson@gmail.com>
2021-01-23 16:47:27 -0800

    Fix exports

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/near-vue-tailwind/'
-        : '/'
-}
+export let publicPath = process.env.NODE_ENV === 'production'
+  ? '/near-vue-tailwind/'
+  : '/';
+
